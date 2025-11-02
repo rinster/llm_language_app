5227c9cdce5ff0efb8da421b5dd2846deeaad3a4
Erine Tieu <enatnat.08@gmail.com>
2025-11-02 14:19:58 -0800

    GET categories API

---
+++ backend/src/main/java/com/teamEleven/backend/controllers/CategoryController.java
@@ -0,0 +1,29 @@
+package com.teamEleven.backend.controllers;
+
+
+import com.teamEleven.backend.entities.Category;
+import com.teamEleven.backend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@CrossOrigin(origins = "http://localhost:3000")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        System.out.println(categories);
+        return ResponseEntity.ok(categories);
+    }
+}
